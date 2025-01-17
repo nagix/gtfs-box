8f3d3a5fbd135314398c99072d020207ff2b4f83
Akihiko Kusanagi <nagi@nagi-p.com>
2025-01-17 12:20:21 +0800

    Add Mini Tokyo 3D Plateau plugin

--- gtfs-box.js
+++ gtfs-box.js
@@ -66,7 +66,7 @@ const matchLang = location.search.match(/lang=(.*?)(?:&|$)/),
         accessToken: 'pk.eyJ1IjoibmFnaXgiLCJhIjoiY201emxlbGhkMDRqYjJxc2IzMnF0dzk5aCJ9.OxzbAxJoC_Myy13ypJ4EeA',
         searchControl: false,
         modeControl: false,
-        plugins: [mt3dPrecipitation()]
+        plugins: [mt3dPrecipitation(), mt3dPlateau({enabled: false})]
     };
 let initialIndex;
 
